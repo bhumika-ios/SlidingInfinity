01af1abba09b95c144764d43db166a63fc0575f8
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-03-31 16:40:32 +0530

    Initial Commit

---
+++ SlidingInfinity/SlidingInfinityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidingInfinityApp.swift
+//  SlidingInfinity
+//
+//  Created by Bhumika Patel on 31/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SlidingInfinityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
