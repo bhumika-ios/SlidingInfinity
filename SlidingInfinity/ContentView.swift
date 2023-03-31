3279fc92a0919d2966f87c79fe85d2eac483b609
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-03-31 17:05:50 +0530

    display image in sliding effect - foreach call model and model call create object store model data under tabview then call page view with data

--- SlidingInfinity/ContentView.swift
+++ SlidingInfinity/ContentView.swift
@@ -5,17 +5,48 @@
 //  Created by Bhumika Patel on 31/03/23.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
+    @State private var pageIndex = 0
+    private let pages: [Page] = Page.samplePages
+    private let dotAppearance = UIPageControl.appearance()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView(selection: $pageIndex) {
+            ForEach(pages) { page in
+                VStack {
+                    Spacer()
+                    PageView(page: page)
+                    Spacer()
+//                    if page == pages.last {
+//                        Button("Sign up", action: goToZero)
+//                            .buttonStyle(.bordered)
+//                    }else {
+//                        Button("next", action: incrementPage)
+//
+//                    }
+                    Spacer()
+                       
+                }
+                .tag(page.tag)
+                
+            }
         }
-        .padding()
+        .animation(.easeInOut, value: pageIndex)
+        .tabViewStyle(.page)
+        .indexViewStyle(.page(backgroundDisplayMode: .interactive))
+        .onAppear {
+            dotAppearance.currentPageIndicatorTintColor = .black
+            dotAppearance.pageIndicatorTintColor = .gray
+        }
+    }
+    func incrementPage() {
+        pageIndex += 1
+    }
+    func goToZero() {
+        pageIndex = 0
     }
 }
 
